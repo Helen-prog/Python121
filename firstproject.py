224aca5365c57120d093a42600eca2dff353e92d
Helen <elena.kozovyakina@gmail.com>
2021-12-02 13:00:57 +0300

    added print

--- firstproject.py
+++ firstproject.py
@@ -2718,24 +2718,4 @@ import time as t
 # f.writelines(s)
 # f.close()
 
-import re
-
-# Найти цифры, рядом с которой нет цифр: 1Х, Text ABC 123 A1B2C3
-text = '1Х, Text ABC 123 A1B2C3'
-reg = r'(?<!\d)\d(?!\d)'
-print(re.findall(reg, text, re.I))
-
-# Найти цифры, рядом с которой нет цифр: Х, Text ABC 123 A1B2C3
-text = 'ABC 123 A1B2C3'
-reg = r'(?<!\d)\d(?!\d)'
-print(re.findall(reg, text, re.I))
-
-# Найти текст от #START# до #END#: text from #START# till #END#
-text = 'from #START# till #END#'
-reg = r'(?<=#START#).*(?=#END#)'
-print(re.findall(reg, text, re.I))
-
-# Найти последовательность цифр, после которой идет ровно одно подчеркивание: 12_34__56
-text = '12_34__56'
-reg = r'\d+(?=_(?!_))'
-print(re.findall(reg, text, re.I))
+print("Вносим изменения")
